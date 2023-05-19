17351a63138490080e199ea31aa6be33483e215f
Ganpat6070 <191200116070@asoit.edu.in>
2023-05-19 18:02:51 +0530

    route redo

--- src/App.js
+++ src/App.js
@@ -46,7 +46,6 @@ function App() {
             <Header mode={mode} setMode={setMode} />
             <Routes>
               <Route path="/" element={<Homepage />} exact></Route>
-              <Route path="/CryptoWorld" element={<Homepage />} exact></Route>
               <Route
                 path="/coins/:id"
                 element={<Fullview mode={mode} setMode={setMode} />}
